4da377d75334e7d2ec29a40d722199f35a9e77c9
hikmatyar22 <abiq5161@gmail.com>
2025-03-21 20:03:09 +0700

    Kode tersebut mengisi **TextBox** bernama **`txtEMAIL`** dengan nilai dari **sel ketiga (indeks 2)** pada baris yang diklik di **DataGridView**.
    Penggunaan **`?.ToString()`** memastikan bahwa jika nilai sel tersebut **`null`**, tidak akan terjadi error (**NullReferenceException**), sehingga aplikasi tetap berjalan lancar meskipun data kosong.

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -236,4 +236,5 @@ namespace CRUDSederhana
                 //Coba gunakan indeks jika "NIM" tidak ditemukan
                 txtNIM.Text = row.Cells[0].Value.ToString();
                 txtNAMA.Text = row.Cells[1].Value?.ToString();
- 
+                txtEMAIL.Text = row.Cells[2].Value?.ToString();
+
