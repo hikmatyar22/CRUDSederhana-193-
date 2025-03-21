aab55b1508168681bf55345a8904e38c73acdb29
hikmatyar22 <abiq5161@gmail.com>
2025-03-21 19:21:41 +0700

    Digunakan untuk menampilkan kotak pesan peringatan dengan tulisan "Harap isi semua data!", tombol OK, dan ikon peringatan (warning).

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -101,4 +101,6 @@ namespace CRUDSederhana
                 try
                 {
                     if (txtNIM.Text == "" || txtNAMA.Text == "" || txtEMAIL.Text == "" || txtTELEPON.Text == "")
-          
+                    {
+                        MessageBox.Show("Harap isi semua data!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        
