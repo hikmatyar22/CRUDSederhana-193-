acc6d4c044cefcaf50d7d4fbf79af918e21d607e
hikmatyar22 <abiq5161@gmail.com>
2025-03-21 19:55:49 +0700

    Kode tersebut mengeksekusi perintah SQL (seperti UPDATE) dan menyimpan jumlah baris yang terpengaruh ke dalam variabel rowsAffected.

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -199,4 +199,5 @@ namespace CRUDSederhana
                         cmd.Parameters.AddWithValue("@Telepon", txtTELEPON.Text.Trim());
                         cmd.Parameters.AddWithValue("@Alamat", txtALAMAT.Text.Trim());
 
-                        
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                      
