bcced66c8abdf721385c5b100b10a10ed7cfcf76
hikmatyar22 <abiq5161@gmail.com>
2025-03-21 19:18:53 +0700

    using SqlConnection conn = new SqlConnection(connectionString); digunakan untuk membuat objek koneksi ke database SQL Server dan memastikan objek tersebut dibersihkan secara otomatis setelah digunakan.

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -96,4 +96,6 @@ namespace CRUDSederhana
 
         private void btnTambah_Click(object sender, EventArgs e)
         {
-           
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+             
