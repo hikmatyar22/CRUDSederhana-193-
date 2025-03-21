02d684faed9ef0ec61c915bbf1dc3f70f4612a9d
hikmatyar22 <abiq5161@gmail.com>
2025-03-21 19:47:35 +0700

    Digunakan untuk menampilkan kotak pesan error dengan tulisan "Error: [pesan error]", tombol OK, dan ikon error, sehingga pengguna tahu ada masalah selama proses penghapusan data.

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -166,4 +166,6 @@ namespace CRUDSederhana
                         }
                         catch (Exception ex)
                         {
-                          
+                            MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+  
