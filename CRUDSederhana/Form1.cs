269e3f665c75a4aa6faeae4f5c609051af988d09
hikmatyar22 <abiq5161@gmail.com>
2025-03-21 16:54:00 +0700

    DataTable dt = new DataTable(); Membuat tabel kosong untuk menampung data yang diambil.

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -49,4 +49,5 @@ namespace CRUDSederhana
                     conn.Open();
                     string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa";
                     SqlDataAdapter da = new SqlDataAdapter(query, conn);
-                    
+                    DataTable dt = new DataTable();
+                   
