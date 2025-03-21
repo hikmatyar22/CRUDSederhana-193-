2f3ccd9ef9279dab872025b34d478b0c3c428336
hikmatyar22 <abiq5161@gmail.com>
2025-03-21 19:59:10 +0700

    Kode tersebut akan menampilkan kotak pesan (MessageBox) dengan judul "Kesalahan", tombol OK, dan ikon Error jika terjadi pengecualian (error).
    Pesan kesalahan ditampilkan dengan menggabungkan teks "Error: " dan pesan error yang didapat dari ex.Message, sehingga pengguna dapat mengetahui penyebab kegagalan secara lebih spesifik.

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -214,4 +214,9 @@ namespace CRUDSederhana
                 }
                 catch (Exception ex)
                 {
-                    
+                    MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+     
