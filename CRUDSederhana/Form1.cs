b3a37b9452dbbc2475bcb18a9662ae5d02ddf2fa
hikmatyar22 <abiq5161@gmail.com>
2025-03-21 19:22:45 +0700

    Digunakan untuk membuka koneksi ke database SQL Server sehingga perintah SQL bisa dijalankan.

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -105,4 +105,5 @@ namespace CRUDSederhana
                         MessageBox.Show("Harap isi semua data!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                         return;
                     }
-           
+                    conn.Open();
+                   
