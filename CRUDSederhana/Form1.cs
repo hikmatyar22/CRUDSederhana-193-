4c78167503a60576a4606d3cb5e7917238e20cab
hikmatyar22 <abiq5161@gmail.com>
2025-03-21 19:46:15 +0700

    Digunakan untuk menangani kondisi ketika jumlah baris yang terpengaruh (rowsAffected) tidak lebih dari nol, yang berarti penghapusan data gagal atau tidak ada data yang terhapus.

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -157,4 +157,7 @@ namespace CRUDSederhana
                                     MessageBox.Show("Data berhasil dihapus!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
                                     LoadData();
                                     ClearForm();
-      
+                                }
+                                else
+                                {
+                                   
